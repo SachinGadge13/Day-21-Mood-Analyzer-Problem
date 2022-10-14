827ca063d4ef3346902d8b6835914e6cc4fa019a
SachinGadge13 <gadges14@gmail.com>
2022-10-15 01:45:02 +0530

    Added:Program of Exception Handling

--- src/com/bridgelabz/MoodAnalyzer/MoodAnalyzer.java
+++ src/com/bridgelabz/MoodAnalyzer/MoodAnalyzer.java
@@ -1,19 +1,24 @@
 package com.bridgelabz.MoodAnalyzer;
 
 public class MoodAnalyzer {
-	private String message;
+    private String message;
 
-	public MoodAnalyzer() {
-	}
+    public MoodAnalyzer() {
+    }
 
-	public MoodAnalyzer(String message) {
-		this.message = message;
-	}
+    public MoodAnalyzer(String message) {
+        this.message = message;
+    }
 
-	public String analyseMood() {
-		if (this.message.contains("Sad"))
-			return "SAD";
-		else
-			return "HAPPY";
-	}
+    public String analyseMood() {
+        try {
+
+            if (this.message.contains("Sad"))
+                return "SAD";
+            else
+                return "HAPPY";
+        } catch (NullPointerException e) {
+            return "HAPPY";
+        }
+    }
 }
